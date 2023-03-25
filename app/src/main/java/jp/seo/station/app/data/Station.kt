900a2651923d5e8448921936c03f2e1d887f190f
k_senda <k_senda@yumemi.co.jp>
2023-03-26 02:24:34 +0900

    fix: station model

--- app/src/main/java/jp/seo/station/app/data/Station.kt
+++ app/src/main/java/jp/seo/station/app/data/Station.kt
@@ -1,18 +1,27 @@
 package jp.seo.station.app.data
 
+import jp.seo.diagram.core.Point
 import jp.seo.diagram.core.VoronoiDiagram.VoronoiArea
 import jp.seo.station.app.data.geo.VoronoiSerializer
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Station(
+class Station(
     val lat: Double,
     val lng: Double,
     val code: Int,
     val name: String,
     var right: Int? = null,
     var left: Int? = null,
-    var next: List<Int>? = null,
+    var next: MutableList<Int>? = null,
     @Serializable(with = VoronoiSerializer::class)
     var voronoi: VoronoiArea? = null,
-)
+): Point() {
+    override fun getX() = lng
+
+    override fun getY() = lat
+
+    override fun toString(): String {
+        return "$name($code)"
+    }
+}
