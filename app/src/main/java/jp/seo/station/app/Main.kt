997a78f951bce1b84adeea71ccb468328918c847
k_senda <k_senda@yumemi.co.jp>
2023-04-15 19:55:58 +0900

    fix: Main

--- app/src/main/java/jp/seo/station/app/Main.kt
+++ app/src/main/java/jp/seo/station/app/Main.kt
@@ -1,13 +1,11 @@
 package jp.seo.station.app
 
-import jp.seo.diagram.core.KdTree
 import jp.seo.diagram.core.KdTree.Node
-import jp.seo.diagram.core.Rectangle
-import jp.seo.diagram.core.VoronoiDiagram
-import jp.seo.station.app.data.Result
+import jp.seo.diagram.spherical.DelaunayDiagram
+import jp.seo.diagram.spherical.LatLng
+import jp.seo.diagram.spherical.Triangle
 import jp.seo.station.app.data.Station
 import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
 
@@ -25,33 +23,17 @@ private fun calc(srcFile: String, dstFile: String) {
     val stations = json.decodeFromString<List<Station>>(src)
     println("station size: ${stations.size}")
 
-    val diagram = VoronoiDiagram(stations)
-    diagram.split(Rectangle(112.0, 60.0, 160.0, 20.0))
+    val points = stations.map { LatLng.euler(it.lat, it.lng) }
+    val diagram = DelaunayDiagram(points)
+    diagram.split(
+        Triangle.points(
+            LatLng.euler(0.0, 136.0),
+            LatLng.euler(50.0, 100.0),
+            LatLng.euler(50.0, 172.0),
+        )
+    )
     println("edge size: ${diagram.edges.size}")
 
-    stations.forEach {
-        it.voronoi = diagram.getVoronoiArea(it)
-        it.next = mutableListOf()
-    }
-    diagram.delaunayEdges.forEach {
-        val s1 = it.a
-        val s2 = it.b
-        if (s1 !is Station) throw ClassCastException()
-        if (s2 !is Station) throw ClassCastException()
-        s1.next?.add(s2.code)
-        s2.next?.add(s1.code)
-    }
-
-    println("build Kd-tree")
-    val tree = KdTree(stations)
-    tree.root.traverseTree()
-
-    val result = Result(
-        root = tree.root.point.code,
-        nodes = stations,
-    )
-    val dst = json.encodeToString(result)
-    File(dstFile).writeText(dst)
 }
 
 private fun Node<Station>.traverseTree() {
