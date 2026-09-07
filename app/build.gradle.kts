721f716b1945cf81cf8a15de8e1af98b9a39f8ec
Seo-4d696b75 <s.kaoru509@gmail.com>
2026-09-08 08:10:05 +0900

    feat: upgrade Gradle to 9.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
 
 dependencies {
     implementation("org.jetbrains:annotations:19.0.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.test {
