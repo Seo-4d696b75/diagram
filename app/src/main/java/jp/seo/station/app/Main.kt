7e551e9966e848558409510bdf137c4b6c109133
Seo-4d696b75 <s.kaoru509@gmail.com>
2026-09-08 07:28:27 +0900

    fix: serializer を明示的に指定する
    ビルド通らない場合がある

--- app/src/main/java/jp/seo/station/app/Main.kt
+++ app/src/main/java/jp/seo/station/app/Main.kt
@@ -8,7 +8,7 @@ import jp.seo.station.app.data.RawStation
 import jp.seo.station.app.data.Result
 import jp.seo.station.app.data.Station
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.encodeToString
+import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
 import java.io.File
 
@@ -25,9 +25,9 @@ private fun calc(srcFile: String, dstFile: String) {
         explicitNulls = false
     }
     val src = File(srcFile).readText()
-    val input = json.decodeFromString<List<RawStation>>(src)
+    val input = json.decodeFromString(ListSerializer(RawStation.serializer()), src)
     val result = input.calc()
-    val dst = json.encodeToString(result)
+    val dst = json.encodeToString(Result.serializer(), result)
     File(dstFile).writeText(dst)
 }
 
