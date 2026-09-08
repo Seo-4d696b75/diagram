6a89a5b64233e613a91f6c1375e741c1e487d56a
Seo-4d696b75 <s.kaoru509@gmail.com>
2026-09-08 12:09:23 +0900

    feat: maven publish plugin 変更
    com.vanniktech.maven.publish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("application")
+    alias(libs.plugins.publish)
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
 }
@@ -19,4 +20,41 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates(
+        groupId = "com.seo4d696b75.diagram",
+        artifactId = "tool-station",
+        version = libs.versions.publish.get(),
+    )
+
+    pom {
+        name.set("2d Diagram Calculation")
+        description.set("Tools to calculate delaunay and voronoi diagram of station coordinates.")
+        url.set("https://github.com/Seo-4d696b75/diagram")
+
+        licenses {
+            license {
+                name = "The Apache Software License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "repo"
+            }
+        }
+
+        developers {
+            developer {
+                id = "seo4d696b75"
+                name = "Seo-4d696b75"
+                email = "s.kaoru509@gmail.com"
+            }
+        }
+
+        scm {
+            url.set("https://github.com/Seo-4d696b75/diagram")
+        }
+    }
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    id("java-library")
+    alias(libs.plugins.publish)
     alias(libs.plugins.kotlin.jvm)
 }
 
@@ -8,4 +10,41 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates(
+        groupId = "com.seo4d696b75.diagram",
+        artifactId = "core",
+        version = libs.versions.publish.get(),
+    )
+
+    pom {
+        name.set("2d Diagram Calculation")
+        description.set("Simple implementations to calculate delaunay and voronoi diagram in 2-dimensional orthonormal system.")
+        url.set("https://github.com/Seo-4d696b75/diagram")
+
+        licenses {
+            license {
+                name = "The Apache Software License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "repo"
+            }
+        }
+
+        developers {
+            developer {
+                id = "seo4d696b75"
+                name = "Seo-4d696b75"
+                email = "s.kaoru509@gmail.com"
+            }
+        }
+
+        scm {
+            url.set("https://github.com/Seo-4d696b75/diagram")
+        }
+    }
 }
